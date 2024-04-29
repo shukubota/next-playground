809ac33c094cf0421dbfb2596d011f0147b0fd1f
shukubota <kubotashu0421@gmail.com>
2024-04-30 02:04:37 +0900

    :fire:

--- pages/performance/index.tsx
+++ pages/performance/index.tsx
@@ -2,7 +2,6 @@ import {HooksDemo} from "../../components/hooksDemo";
 import {GetServerSideProps} from "next";
 
 const PerformanceDemo = (props: any) => {
-  console.log('-------PerformanceDemo');
   return (
     <div>
       <h1>Performance Demo</h1>
@@ -19,9 +18,6 @@ type Repo = {
 export const getServerSideProps = (async (context) => {
     const res = await fetch('https://api.github.com/repos/vercel/next.js')
     const repo = await res.json();
-
-    console.log('------getServerSideProps');
-    // console.log({ repo });
     return { props: { repo } }
 }) satisfies GetServerSideProps<{
     repo: Repo
