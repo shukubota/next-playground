c00eaefc87d81a5956e226af5a06ee8706278971
shukubota <kubotashu0421@gmail.com>
2024-04-30 02:01:48 +0900

    server props

--- pages/performance/index.tsx
+++ pages/performance/index.tsx
@@ -9,4 +9,17 @@ const PerformanceDemo = () => {
   )
 }
 
+export async function getServerSideProps() {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js');
+  const repo = await res.json();
+  return {
+    props: {
+      repo: {
+        name: repo.name,
+        description: repo.description,
+      }
+    }
+  }
+}
+
 export default PerformanceDemo;
