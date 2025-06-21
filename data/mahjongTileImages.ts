fefc77eaef7ee4639ee305695c669975f5531a6c
shukubota <kubotashu0421@gmail.com>
2025-06-22 03:20:22 +0900

    add 画像

---
+++ data/mahjongTileImages.ts
@@ -0,0 +1,44 @@
+// 麻雀牌の画像パスのマッピング
+export const tileImageMap: Record<string, string> = {
+  // 萬子 (m)
+  '1m': '/images/mahjong-tiles/man1.svg',
+  '2m': '/images/mahjong-tiles/man2.svg',
+  '3m': '/images/mahjong-tiles/man3.svg',
+  '4m': '/images/mahjong-tiles/man4.svg',
+  '5m': '/images/mahjong-tiles/man5.svg',
+  '6m': '/images/mahjong-tiles/man6.svg',
+  '7m': '/images/mahjong-tiles/man7.svg',
+  '8m': '/images/mahjong-tiles/man8.svg',
+  '9m': '/images/mahjong-tiles/man9.svg',
+  
+  // 筒子 (p)
+  '1p': '/images/mahjong-tiles/pin1.svg',
+  '2p': '/images/mahjong-tiles/pin2.svg',
+  '3p': '/images/mahjong-tiles/pin3.svg',
+  '4p': '/images/mahjong-tiles/pin4.svg',
+  '5p': '/images/mahjong-tiles/pin5.svg',
+  '6p': '/images/mahjong-tiles/pin6.svg',
+  '7p': '/images/mahjong-tiles/pin7.svg',
+  '8p': '/images/mahjong-tiles/pin8.svg',
+  '9p': '/images/mahjong-tiles/pin9.svg',
+  
+  // 索子 (s)
+  '1s': '/images/mahjong-tiles/sou1.svg',
+  '2s': '/images/mahjong-tiles/sou2.svg',
+  '3s': '/images/mahjong-tiles/sou3.svg',
+  '4s': '/images/mahjong-tiles/sou4.svg',
+  '5s': '/images/mahjong-tiles/sou5.svg',
+  '6s': '/images/mahjong-tiles/sou6.svg',
+  '7s': '/images/mahjong-tiles/sou7.svg',
+  '8s': '/images/mahjong-tiles/sou8.svg',
+  '9s': '/images/mahjong-tiles/sou9.svg',
+  
+  // 字牌
+  '東': '/images/mahjong-tiles/east.svg',
+  '南': '/images/mahjong-tiles/south.svg',
+  '西': '/images/mahjong-tiles/west.svg',
+  '北': '/images/mahjong-tiles/north.svg',
+  '白': '/images/mahjong-tiles/haku.svg',
+  '發': '/images/mahjong-tiles/hatsu.svg',
+  '中': '/images/mahjong-tiles/chun.svg',
+};
